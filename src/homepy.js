08b4fc7112b355ad75e5ba99fea01d3da60c0344
Your Name <okodee@naver.com>
2024-05-21 21:56:19 +0900

    feat: Add touchpoint toast & vomvom modal

--- src/homepy.js
+++ src/homepy.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import "./style.css";
-import "./styleguide.css";
+import "./homepy-style.css";
+import "./homepy-styleguide.css";
 // import SVGInline from 'react-svg-inline'
+
+import Search from "./assets/search.svg";
+import Ph_bell_light from "./assets/ph-bell-light.svg";
+import Ic_baseline_people from "./assets/ic-baseline-people.svg";
+import Ic_outline_email from "./assets/ic-outline-email.svg";
+import Mingcute_birthday from "./assets/mingcute-birthday-2-line.svg";
+import Fluent_person from "./assets/fluent-person-12-regular.svg";
+import Mingcute_location from "./assets/mingcute-location-line.svg";
+import Mdi_heart from "./assets/mdi-heart.svg";
+import Ic_baseline_people_white from "./assets/ic-baseline-people-white.svg";
+import Icon from "./assets/icon-50.svg";
+import Icon_24 from "./assets/icon_24.svg";
+
 
 export default function main() {
     return (
@@ -11,14 +24,14 @@ export default function main() {
                     <div className="frame">
                         <div className="input-no-label">
                             <div className="input-no-label-2">
-                                <img className="ic-baseline-people" alt="Search" src="search.svg" />
+                                <img className="ic-baseline-people" alt="Search" src={Search} />
                                 <div className="label">닉네임을 검색해보세요</div>
                             </div>
                         </div>
                         <div className="input-no-label-3">
-                            <img className="img" alt="Ph bell light" src="ph-bell-light.svg" />
+                            <img className="img" alt="Ph bell light" src={Ph_bell_light} />
                         </div>
-                        <img className="mask-group" alt="Mask group" src="mask-group.png" />
+                        <img className="mask-group" alt="Mask group" src={require("./assets/Mask-group.png")} />
                     </div>
                     <div className="text-wrapper">VOM</div>
                 </header>
@@ -39,6 +52,62 @@ export default function main() {
                                 </p>
                             </div>
                         </div>
+
+
+
+
+                        <div className="toast">
+                            <div className="frame-toast">
+                                <img className="mdi-heart-toast" alt="Mdi heart" src={Mdi_heart} />
+                                <div className="div-wrapper-toast">
+                                    <div className="text-wrapper-2-toast">박서현 님에게 관심을 보냈습니다</div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div className="modal">
+                            <div className="text-wrapper-modal">봄봄 신청</div>
+                            <img className="icon-modal" alt="Icon" src={Icon_24} />
+                            <div className="div-modal">박서현 님에게 봄봄을 신청할까요?</div>
+                            <button className="button-modal">
+                                <div className="text-wrapper-2-modal">신청</div>
+                            </button>
+                            <button className="div-wrapper-modal">
+                                <div className="text-wrapper-3-modal">취소</div>
+                            </button>
+                        </div>
+
+
+
+
+
+                        <div className="frame-vomvom">
+                            <div className="overlap-group-vomvom">
+                                <div className="menu-vomvom">
+                                    <div className="menu-item-vomvom">
+                                        <div className="div-vomvom">
+                                            <img className="ellipse-vomvom" alt="Ellipse" src={require("./assets/ellipse-1495.png")} />
+                                            <div className="text-wrapper-vomvom">오수아</div>
+                                        </div>
+                                    </div>
+                                    <div className="menu-item-vomvom">
+                                        <div className="div-vomvom">
+                                            <img className="ellipse-vomvom" alt="Ellipse" src={require("./assets/ellipse-1496.png")} />
+                                            <div className="text-wrapper-vomvom">최수빈</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="rectangle-vomvom" />
+                            </div>
+                        </div>
+
+
+
+
                         <div className="frame-6">
                             <div className="text-wrapper-3">관심 키워드</div>
                             <div className="frame-7">
@@ -85,32 +154,32 @@ export default function main() {
                             <div className="frame-wrapper">
                                 <div className="frame-10">
                                     <div className="unsplash-wrapper">
-                                        <img className="unsplash" alt="Unsplash" src="unsplash-oqhyqygd9po.png" />
+                                        <img className="unsplash" alt="Unsplash" src={require("./assets/unsplash-oqHYQyGD9Po.png")} />
                                     </div>
                                     <div className="unsplash-wrapper">
-                                        <img className="unsplash" alt="Unsplash" src="unsplash-r2njpbeyusq.png" />
+                                        <img className="unsplash" alt="Unsplash" src={require("./assets/unsplash-r2nJPbEYuSQ.png")} />
                                     </div>
                                     <div className="unsplash-wrapper">
-                                        <img className="unsplash" alt="Unsplash" src="unsplash-kmn4veeepr8.png" />
+                                        <img className="unsplash" alt="Unsplash" src={require("./assets/unsplash-KMn4VEeEPR8.png")} />
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <button className="button">
-                        <img className="img-2" alt="Icon" src="icon-50.svg" />
+                        <img className="img-2" alt="Icon" src={Icon} />
                         <div className="text-wrapper-4">화상채팅 시작하기</div>
                     </button>
                 </div>
                 <div className="frame-11">
                     <div className="frame-12">
-                        <img className="mask-group-2" alt="Mask group" src="high-school-student-teenager-girl-headphones-with-tablet-home 1.png" />
+                        <img className="mask-group-2" alt="Mask group" src={require("./assets/high-school-student-teenager-girl-headphones-with-tablet-home 1.png")} />
                         <div className="frame-13">
                             <div className="frame-14">
                                 <div className="text-wrapper-5">박서현</div>
                                 <div className="frame-15">
                                     <div className="frame-16">
-                                        <img className="ic-baseline-people" alt="Ic baseline people" src="ic-baseline-people.svg" />
+                                        <img className="ic-baseline-people" alt="Ic baseline people" src={Ic_baseline_people} />
                                         <div className="text-wrapper-6">봄봄</div>
                                     </div>
                                     <div className="text-wrapper-3">32</div>
@@ -119,28 +188,28 @@ export default function main() {
                             <div className="frame-7">
                                 <div className="frame-17">
                                     <div className="frame-18">
-                                        <img className="img-3" alt="Ic outline email" src="ic-outline-email.svg" />
+                                        <img className="img-3" alt="Ic outline email" src={Ic_outline_email} />
                                         <div className="text-wrapper-7">이메일</div>
                                     </div>
                                     <div className="text-wrapper-8">baek789@naver.com</div>
                                 </div>
                                 <div className="frame-17">
                                     <div className="frame-18">
-                                        <img className="img-3" alt="Mingcute birthday" src="mingcute-birthday-2-line.svg" />
+                                        <img className="img-3" alt="Mingcute birthday" src={Mingcute_birthday} />
                                         <div className="text-wrapper-7">생년월일</div>
                                     </div>
                                     <div className="text-wrapper-8">2023-12-31</div>
                                 </div>
                                 <div className="frame-17">
                                     <div className="frame-19">
-                                        <img className="img-3" alt="Fluent person" src="fluent-person-12-regular.svg" />
+                                        <img className="img-3" alt="Fluent person" src={Fluent_person} />
                                         <div className="text-wrapper-7">나이</div>
                                     </div>
                                     <div className="text-wrapper-8">20세</div>
                                 </div>
                                 <div className="frame-17">
                                     <div className="frame-18">
-                                        <img className="img-3" alt="Mingcute location" src="mingcute-location-line.svg" />
+                                        <img className="img-3" alt="Mingcute location" src={Mingcute_location} />
                                         <div className="text-wrapper-7">지역</div>
                                     </div>
                                     <div className="text-wrapper-8">서울</div>
@@ -150,11 +219,11 @@ export default function main() {
                     </div>
                     <div className="frame-20">
                         <button className="button-2">
-                            <img className="img-2" alt="Mdi heart" src="mdi-heart.svg" />
+                            <img className="img-2" alt="Mdi heart" src={Mdi_heart} />
                             <div className="text-wrapper-9">관심 보내기</div>
                         </button>
                         <button className="button-3">
-                            <img className="img-2" alt="Ic baseline people" src="ic-baseline-people-white.svg" />
+                            <img className="img-2" alt="Ic baseline people" src={Ic_baseline_people_white} />
                             <div className="text-wrapper-4">봄봄 신청하기</div>
                         </button>
                     </div>
