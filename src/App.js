e60a5809c1e24a50da678acfa3812a7ef9170b63
Ojimin <ogm0205@gmail.com>
2024-05-22 01:08:44 +0900

    feat: 로그인 페이지 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginPage from "../src/pages/LoginPage";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginCallback from "../src/components/LoginPage/LoginCallback";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* 기본 경로를 /login으로 설정 */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/auth/callback/google" element={<LoginCallback />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
